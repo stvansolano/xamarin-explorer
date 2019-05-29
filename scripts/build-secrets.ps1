06c22bbd31b155a3d73d59f53942cfec5016959d
Esteban Solano Granados <stvansolano@outlook.com>
2019-05-28 19:12:36 -0600

    CI pipeline (Azure DevOps) (#23)
    * Set up CI with Azure Pipelines
    
    * Fixed Unit Tests
    
    * Added build-secrets file
    
    * Added empty Secrets.json
    
    * Added Secrets.json back to .gitignore

---
+++ scripts/build-secrets.ps1
@@ -0,0 +1,23 @@
+if($env:PackageFeedPat) {
+    # NuGet config named template.config to prevent Visual Studio from picking it up for local development
+    (Get-Content .\build\template.config) `
+      -replace '%PackageFeedPat%', "$env:PackageFeedPat" |
+    Out-File NuGet.config
+    Write-Host "Generated NuGet.config"
+}
+else {
+    Write-Warning "There is no environment variable defined for the Package Feed"
+}
+
+Write-Host "Downloading latest nuget.exe"
+Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\nuget.exe
+
+Write-Host "Performing NuGet Restore"
+.\nuget.exe restore -ConfigFile .\NuGet.config
+
+# Hack... Windows client capitalizes all environment variables wrecking the Mobile.BuildTools
+$secrets = @{
+    AppCenter_Android_Secret = "$env:AppCenterSecret"
+}
+
+$secrets | ConvertTo-Json | Out-File .\src\XamarinExplorer\secrets.json
