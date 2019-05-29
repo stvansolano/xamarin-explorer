beaa55c4db74b755ac602cca2fab8f486de3147c
Esteban Solano Granados <stvansolano@outlook.com>
2019-05-28 19:59:43 -0600

    Commented lines in build-secrets.ps1

--- scripts/build-secrets.ps1
+++ scripts/build-secrets.ps1
@@ -1,13 +1,13 @@
-if($env:PackageFeedPat) {
+#if($env:PackageFeedPat) {
     # NuGet config named template.config to prevent Visual Studio from picking it up for local development
-    (Get-Content .\build\template.config) `
-      -replace '%PackageFeedPat%', "$env:PackageFeedPat" |
-    Out-File NuGet.config
-    Write-Host "Generated NuGet.config"
-}
-else {
-    Write-Warning "There is no environment variable defined for the Package Feed"
-}
+#    (Get-Content .\build\template.config) `
+#      -replace '%PackageFeedPat%', "$env:PackageFeedPat" |
+#    Out-File NuGet.config
+#    Write-Host "Generated NuGet.config"
+#}
+#else {
+#    Write-Warning "There is no environment variable defined for the Package Feed"
+#}
 
 Write-Host "Downloading latest nuget.exe"
 Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\nuget.exe
