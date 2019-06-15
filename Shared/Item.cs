6138dcb04a923df8a0ccff151a70470247bf5dd4
Esteban Solano Granados <stvansolano@outlook.com>
2019-06-14 19:06:15 -0600

    Updated ItemsController methods

--- Shared/Item.cs
+++ Shared/Item.cs
@@ -4,7 +4,7 @@ namespace Shared
 {
 	public class Item
 	{
-		public string Id { get; set; }
+		public int Id { get; set; }
 		public string Text { get; set; }
 		public string Description { get; set; }
 	}
