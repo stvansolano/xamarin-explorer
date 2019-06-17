ffea7cd779dfd99602eded04674c16878d4e39b1
Esteban Solano Granados <stvansolano@outlook.com>
2019-06-17 12:39:38 -0600

    Updated Android project

--- XamarinExplorer.Android/MainActivity.cs
+++ XamarinExplorer.Android/MainActivity.cs
@@ -11,44 +11,48 @@ using System.Threading.Tasks;
 
 namespace XamarinExplorer.Droid
 {
-	[Activity(Label = "Xamarin Explorer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			AppDomain currentDomain = AppDomain.CurrentDomain;
-			currentDomain.UnhandledException += HandleExceptions;
-			TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;  
+    [Activity(Label = "Xamarin Explorer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += HandleExceptions;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
 
-			TabLayoutResource = Resource.Layout.Tabbar;
-			ToolbarResource = Resource.Layout.Toolbar;
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
 
-			base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
-			Xamarin.Forms.Forms.SetFlags("FastRenderers_Experimental");
-			global::Xamarin.Forms.Forms.Init(this, bundle);
-			LoadApplication(new App());
-		}
+            Xamarin.Forms.Forms.SetFlags("FastRenderers_Experimental");
 
-		private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
-		{
-			Crashes.TrackError(e.Exception);
-		}
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
 
-		private void HandleExceptions(object sender, UnhandledExceptionEventArgs e)
-		{
-			var innerException = e.ExceptionObject as Exception;
-			if (innerException == null)
-			{
-				return;
-			}
-			Crashes.TrackError(innerException);
-		}
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Crashes.TrackError(e.Exception);
+        }
 
-		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
-		{
-			Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-		}
-	}
+        private void HandleExceptions(object sender, UnhandledExceptionEventArgs e)
+        {
+            var innerException = e.ExceptionObject as Exception;
+            if (innerException == null)
+            {
+                return;
+            }
+            Crashes.TrackError(innerException);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            //Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
-
