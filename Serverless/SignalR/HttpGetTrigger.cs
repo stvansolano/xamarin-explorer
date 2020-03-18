e29982a86cddd9363a7c9457fa79997d2b77bb2b
Esteban Solano G <stvansolano@outlook.com>
2020-03-18 16:41:29 -0600

    Added MongoDB functions

--- Serverless/SignalR/HttpGetTrigger.cs
+++ Serverless/SignalR/HttpGetTrigger.cs
@@ -1,35 +1,45 @@
 using System;
-using System.IO;
+using System.Security.Authentication;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 
 namespace Serverless
 {
-    public static class HttpGetTrigger
+    public static class ToDoGet
     {
-        [FunctionName("HttpGetTrigger")]
+        [FunctionName("ToDoGet")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var connection = Environment.GetEnvironmentVariable("MongoDbConnection");
+            var databaseName = Environment.GetEnvironmentVariable("MongoDbDatabase");
+            var collectionName = Environment.GetEnvironmentVariable("MongoDbCollection");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+                new MongoUrl(connection)
+            );
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
 
-            return new OkObjectResult(responseMessage);
+            var mongoClient = new MongoClient(settings);
+            var client = new MongoClient(connection);
+            var database = client.GetDatabase(databaseName);
+
+            IMongoCollection<MyToDo> collection = database.GetCollection<MyToDo>(collectionName);
+
+            var result = await collection.Find(FilterDefinition<MyToDo>.Empty)
+                                         .ToListAsync<MyToDo>();
+
+            return new OkObjectResult(JsonConvert.SerializeObject(result));
         }
     }
 }
